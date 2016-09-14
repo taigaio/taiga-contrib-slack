776c40ee1359848d091ac731b84ed2557d4c91d7
Alejandro Alonso <alejandroalonsofernandez@gmail.com>
2016-09-14 16:21:20 +0200

    Epics support

--- back/taiga_contrib_slack/models.py
+++ back/taiga_contrib_slack/models.py
@@ -25,6 +25,13 @@ class SlackHook(models.Model):
     url = models.URLField(null=False, blank=False, verbose_name=_("URL"))
     channel = models.CharField(null=True, blank=True, verbose_name=_("Channel"), max_length=200)
 
+    notify_epic_create = models.BooleanField(default=True)
+    notify_epic_change = models.BooleanField(default=True)
+    notify_epic_delete = models.BooleanField(default=True)
+
+    notify_relateduserstory_create = models.BooleanField(default=True)
+    notify_relateduserstory_delete = models.BooleanField(default=True)
+
     notify_issue_create = models.BooleanField(default=True)
     notify_issue_change = models.BooleanField(default=True)
     notify_issue_delete = models.BooleanField(default=True)
