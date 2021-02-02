4ea2ff587993e6ea40ef816809e13e7806318326
David Barragán Merino <david.barragan@kaleidos.net>
2021-02-02 08:13:28 +0100

    Update version

--- back/taiga_contrib_slack/__init__.py
+++ back/taiga_contrib_slack/__init__.py
@@ -15,6 +15,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = (5, 5, 1)
+__version__ = (6, 0, 0)
 
 default_app_config = "taiga_contrib_slack.apps.TaigaContribSlackAppConfig"
