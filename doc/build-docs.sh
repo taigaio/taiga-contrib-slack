c6956f4afbd8f7c7126613f186b02015ec2540e9
David Barragán Merino <david.barragan@kaleidos.net>
2024-03-27 12:41:31 +0100

    fix: company name: Kaleidos Ventures SL now is Kaleidos INC

--- doc/build-docs.sh
+++ doc/build-docs.sh
@@ -4,7 +4,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# Copyright (c) 2021-present Kaleidos Ventures SL
+# Copyright (c) 2021-present Kaleidos INC
 
 make || exit 1
 rm -rf /tmp/taiga-contrib-slack-doc-dist || exit 1
